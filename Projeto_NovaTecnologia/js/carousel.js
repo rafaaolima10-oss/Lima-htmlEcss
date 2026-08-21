1493b2d8a42360c6805f5735f1f6219ca5ec22ce
rafaaolima10-oss <rafaaolima10@gmail.com>
2026-08-21 16:45:19 -0300

    Projeto finalizado

--- Projeto_NovaTecnologia/js/carousel.js
+++ Projeto_NovaTecnologia/js/carousel.js
@@ -1,26 +1,29 @@
-document.addEventListener("DOMContentLoaded", listener = () => {
+document.addEventListener("DOMContentLoaded", () => {
     const prevButton = document.querySelector(".carousel-btn.prev");
     const nextButton = document.querySelector(".carousel-btn.next");
     const carouselContainer = document.querySelector(".carousel-container");
-    const carouselItems = document.querySelector(".carousel-Item");
+    const carouselItems = document.querySelectorAll(".carousel-item");
     const totalItems = carouselItems.length;
     let currentIndex = 0;
-
+ 
     const changeSlide = () => {
-        carouselContainer.style.transform = `translateX(-${currentIndex * 100})`
+        carouselContainer.style.transform = `translateX(-${currentIndex *100}%)`;
     }
     const nextSlide = () => {
-        currentIndex = (currentIndex + 1) % totalItems;
-        changeSlide();
-    } 
-    const prevSlide = () =>{
-        currentIndex = (currentIndex - 1 totalItems);
+        currentIndex = (currentIndex + 1 ) % totalItems;
         changeSlide();
     }
-    nextButton.addEventListener("click; nextSlide");
-    prevButton.addEventListener("click; nextSlide");
-
+    const prevSlide = () => {
+        currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+        changeSlide();
+    }
+    nextButton.addEventListener("click", nextSlide);
+    prevButton.addEventListener("click", prevSlide);
+   
     setInterval(nextSlide, 7000);
-
+ 
     changeSlide();
+ 
 })
+ 
+ 
